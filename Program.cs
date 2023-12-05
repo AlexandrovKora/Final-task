789d2e6bbeeb904c0b75f9b37500e6ae1af1c25c
Mishka <lp-fan1992@mail.ru>
2023-12-05 18:44:11 +0300

    Programm

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿void Final_task()
+
+{
+
+Console.WriteLine("Введите массив (элементы отделяйте запятой): ");
+
+string arrayElements = Console.ReadLine();
+
+string[] commonArray = arrayElements.Split(",");
+
+string[] finalArray = commonArray.Where(arrayElements => arrayElements.Length < 4).ToArray();
+
+Console.WriteLine("Итоговый массив из элементовтов, состоящих из 3 меньше элементов:  ");
+
+Console.WriteLine(String.Join(", ", finalArray));
+
+}
+
+Final_task();
